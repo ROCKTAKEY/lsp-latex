898736a05b885819cf34919945445845dab5a8c6
ROCKTAKEY <rocktakey@gmail.com>
2019-04-08 22:43:34 +0900

    Override `completing-read' on test.

--- test/lsp-latex-test.el
+++ test/lsp-latex-test.el
@@ -32,7 +32,7 @@
 
 (ert-deftest lsp-latex-open ()
   "Test for lsp-latex."
-  (noflet ((lsp--select-action (arg) (car arg)))
+  (noflet ((completing-read (a b c d) (message a) (car b)))
    (add-to-list 'exec-path "~/")
   (find-file "/sample-for-latex/test.tex")
   (lsp)))
